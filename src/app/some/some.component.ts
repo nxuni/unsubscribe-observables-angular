7f8e2e5ffdac3e9f8866e9b4e11cd0efebd5d223
Carlos A Benitez M <ca.benitez.m@msn.com>
2022-06-21 17:57:54 -0500

    Using Async | Pipe

--- src/app/some/some.component.ts
+++ src/app/some/some.component.ts
@@ -6,35 +6,17 @@ import { interval, Observable, Subscription } from 'rxjs';
   templateUrl: './some.component.html',
   styleUrls: ['./some.component.css']
 })
-export class SomeComponent implements OnInit, OnDestroy {
+export class SomeComponent implements OnInit {
   @Input() instanceNumber = 0;
+  subscription1 = new Observable<number>;
+  subscription2 = new Observable<number>;
 
-  subscription1: Subscription | undefined
-  x1: number | undefined;
-
-  subscription2: Subscription | undefined;
-  x2: number | undefined;
-
-  parentSubscription = new Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.subscription1 = interval(100).subscribe(x => {
-      this.x1 = x;
-      console.log(this.instanceNumber + " - 1 - " + this.x1);
-    });
-    this.parentSubscription.add(this.subscription1);
+    this.subscription1 = interval(100);
 
-    this.subscription2 = interval(100).subscribe(x => {
-      this.x2 = x;
-      console.log(this.instanceNumber + " - 2 - " + this.x2);
-    });
-    this.parentSubscription.add(this.subscription2);
-  }
-
-  ngOnDestroy(): void {
-    this.parentSubscription.unsubscribe();
-    console.log("subscriptions unsubscribed");
+    this.subscription2 = interval(100);
   }
 }
