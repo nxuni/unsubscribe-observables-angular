5451483e769fe18e35435d08056da8efba38f8b1
Carlos A Benitez M <ca.benitez.m@msn.com>
2022-06-21 18:26:50 -0500

    using takeUntil

--- src/app/some/some.component.ts
+++ src/app/some/some.component.ts
@@ -1,22 +1,39 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { interval, Observable, Subscription } from 'rxjs';
+import { interval, Observable, Subject, Subscription, take, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-some',
   templateUrl: './some.component.html',
   styleUrls: ['./some.component.css']
 })
-export class SomeComponent implements OnInit {
+export class SomeComponent implements OnInit, OnDestroy {
   @Input() instanceNumber = 0;
-  subscription1 = new Observable<number>;
-  subscription2 = new Observable<number>;
 
+  subscription1: Subscription | undefined
+  x1: number | undefined;
+
+  subscription2: Subscription | undefined;
+  x2: number | undefined;
+
+  notifier = new Subject();
 
   constructor() { }
 
   ngOnInit(): void {
-    this.subscription1 = interval(100);
+    this.subscription1 = interval(100).pipe(takeUntil(this.notifier)).subscribe(x => {
+      this.x1 = x;
+      console.log(this.instanceNumber + " - 1 - " + this.x1);
+    });
 
-    this.subscription2 = interval(100);
+    this.subscription2 = interval(100).pipe(takeUntil(this.notifier)).subscribe(x => {
+      this.x2 = x;
+      console.log(this.instanceNumber + " - 2 - " + this.x2);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.notifier.next(0);
+    this.notifier.complete();
+    console.log("notifier complete");
   }
 }
